5506bfb93ade8fd714c47566d315c39dec48fae4
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-18 18:29:40 +0100

    Single flight

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def ping():
 def get_flights():
     return jsonify(flights)
 
+@app.route('/flights/<int:id>')
+def get_single_flight(id):
+    flight = [element for element in flights if element['flight_id'] == id]
+    return flight
+
 
 if __name__ == '__main__':
     app.run(debug=True)
