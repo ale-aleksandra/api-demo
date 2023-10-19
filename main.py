a5aaf83ff0f40482bd54eb4c6cbdb4d0c03f068f
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-19 10:04:59 +0100

    Single flight by city of departure

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def ping():
 def get_flights():
     return jsonify(flights)
 
+@app.route('/flights/by_from_city/<string:from_city>')
+def get_single_flight_by_from_city(from_city):
+    flight = [element for element in flights if element['from_city'] == from_city]
+    return flight
+
 @app.route('/flights/<int:id>')
 def get_single_flight(id):
     flight = [element for element in flights if element['flight_id'] == id]
